21dbc3bf792e589e6921bed0e885aa57a677534d
Juan Felipe Duarte <70235683+JuanFelipeDuarte@users.noreply.github.com>
2022-10-08 02:00:38 -0500

    funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 
 function main() {
     return {
-        url: 'https://rickandmortyapi.com/api/character',
-        characters: []
+        url: 'https://rickandmortyapi.com/api/character',   //propiedades
+        characters: [],
+        numPages: 0,
+        page:1,
+        changePage: async function (type) {
+            if (type === 'next' && this.page < this.numPages) {
+                this.page++;
+            }else if(type === 'prev' && this.page > 1){
+                this.page--;
+            }
+            const response = await fetch(`${this.url}?page=${this.page}`);
+            this.characters = (await response.json()).results;
+        }
     }
 }
